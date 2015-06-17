5f5ae7fa88a0fdd2c4562a802927d70a732b4463
dhlee111 <lidonghe111@gmail.com>
2015-06-17 14:36:11 +0900

    matrix 수정

--- alignment.py
+++ alignment.py
@@ -2,7 +2,7 @@ seq1=raw_input("Enter the first sequence: ")
 seq2=raw_input("Enter the second sequence: ")
 
 def Matrix(matrix):
-    mat=[[0 for x in range(matrix[0])] for x in range(matrix[1])]
+    mat=[[0 for x in range(matrix[1])] for x in range(matrix[0])]
     return mat
           
     indel=1
@@ -16,11 +16,12 @@ def grade(a, b):
         return sub
     
 def score(a,b,s):
+    m=len(a)+1   # length of sequence 1
+    n=len(b)+1   # length of sequence 2 
     s1=s[i-1][j-1]+grade(a,b)
     s2=s[i][j-1]+indel
     s3=s[i-1][j]+indel
     s[i][j]=min(s1,s2,s3)
     return s
-
 
 
