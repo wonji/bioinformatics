2b0d316efc142ac2e93a37bdfa1fdd577ffe6501
wonji <dnjswlzz11@gmail.com>
2015-06-17 21:35:45 +0900

    Back trace 추가해서 Merge

--- alignment.py
+++ alignment.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class EditDistance(object):
  def getSeq1(self):
   return self.__seq1
@@ -41,6 +42,43 @@ class EditDistance(object):
   for line in self.__mat: self.result.append(str(line))
   self.result.extend([' ','Minimum Distance :'+' '+str(self.calDistance())])
   print '\n'.join(self.result)
+=======
+seq1=raw_input("Enter the first sequence: ")
+seq2=raw_input("Enter the second sequence: ")
+
+def Matrix(matrix):
+    mat=[[0 for x in range(matrix[1])] for x in range(matrix[0])]
+    return mat
+          
+    indel=1
+    sub=1
+    copy=0
+
+def grade(a,b):
+    if a==b:
+        return copy
+    else:
+        return sub
+    
+def score(a,b):
+    m=len(a)+1   # length of sequence 1
+    n=len(b)+1   # length of sequence 2 
+    tb=Matrix((m,n))
+    s=tb
+
+    for i in range(1,n):            #set the first row and first column 
+        s[0][i]=s[0][i-1]+1
+    for j in range(1,m):
+        s[j][0]=s[j-1][0]+1
+    
+    for i in range(1,m):
+        for j in range(1,n):
+            s1=s[i-1][j-1]+grade(a[i-1],b[j-1]) # score from diagonal
+            s2=s[i][j-1]+indel      #score from left
+            s3=s[i-1][j]+indel      #score from up
+            s[i][j]=min(s1,s2,s3)
+    return s 
+>>>>>>> 307ffb986ae447093528be44df133d2b22c50cf2
 
 
 wonji = EditDistance("GCTGA","GGGTGA")
