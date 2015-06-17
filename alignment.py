1752ad71759fd467929c2467e36ff7b289e6f2c2
wonji <dnjswlzz11@gmail.com>
2015-06-17 21:43:55 +0900

    Back trace 수정중

--- alignment.py
+++ alignment.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 class EditDistance(object):
  def getSeq1(self):
   return self.__seq1
@@ -43,68 +41,11 @@ class EditDistance(object):
   for line in self.__mat: self.result.append(str(line))
   self.result.extend([' ','Minimum Distance :'+' '+str(self.calDistance())])
   print '\n'.join(self.result)
-=======
-=======
->>>>>>> f81d8ba3861f618b39f36ccc2947a369dcd6c1c5
-seq1=raw_input("Enter the first sequence: ")
-seq2=raw_input("Enter the second sequence: ")
-
-def Matrix(matrix):
-    mat=[[0 for x in range(matrix[1])] for x in range(matrix[0])]
-    return mat
-          
-    indel=1
-    sub=1
-    copy=0
-
-def grade(a,b):
-    if a==b:
-        return copy
-    else:
-        return sub
-    
-def score(a,b):
-    m=len(a)+1   # length of sequence 1
-    n=len(b)+1   # length of sequence 2 
-<<<<<<< HEAD
-    tb=Matrix((m,n))
-    s=tb
-=======
-    s=Matrix((m,n))
-    arrow=Matrix((m,n))
->>>>>>> f81d8ba3861f618b39f36ccc2947a369dcd6c1c5
-
-    for i in range(1,n):            #set the first row and first column 
-        s[0][i]=s[0][i-1]+1
-    for j in range(1,m):
-        s[j][0]=s[j-1][0]+1
-<<<<<<< HEAD
-=======
-
-    for i in range(1,n):
-        a
-
-
->>>>>>> f81d8ba3861f618b39f36ccc2947a369dcd6c1c5
-    
-    for i in range(1,m):
-        for j in range(1,n):
-            s1=s[i-1][j-1]+grade(a[i-1],b[j-1]) # score from diagonal
-            s2=s[i][j-1]+indel      #score from left
-            s3=s[i-1][j]+indel      #score from up
-            s[i][j]=min(s1,s2,s3)
-<<<<<<< HEAD
-    return s 
->>>>>>> 307ffb986ae447093528be44df133d2b22c50cf2
 
 
 wonji = EditDistance("GCTGA","GGGTGA")
 wonji.calDistance()
 wonji.showResult()
-=======
-    return s
-
-
 
 i,j=m-1,n-1
 oo=''
@@ -137,11 +78,3 @@ def backtrack(s):
         return oo
     else: 
         return backtrack(s)
-
-
-
-
-
-
-
->>>>>>> f81d8ba3861f618b39f36ccc2947a369dcd6c1c5
