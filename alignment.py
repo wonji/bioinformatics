14dcc318a0b84fd0427f9ee14e2901bc1c5b2c5b
wonji <dnjswlzz11@gmail.com>
2015-06-17 16:51:36 +0900

    OOP 수정완료

--- alignment.py
+++ alignment.py
@@ -1,53 +1,21 @@
 class EditDistance(object):
- __seq1="" #±‚¡∏¿« sequence#
- __seq2="" #πŸ≤Ô sequence#
- indel=1
- sub=1
- copy=0
+ def getSeq1(self):
+  return self.__seq1
+ def setSeq1(self,value):
+  self.__seq1=value
+ def getSeq2(self):
+  return self.__seq2
+ def setSeq2(self,value):
+  self.__seq2=value
  def __init__(self, seq1, seq2):
-  self.__seq1=seq1
-  self.__seq2=seq2
+  self.setSeq1(seq1)
+  self.setSeq2(seq2)
  def Matrix(self, matrix):
-  mat=[[0 for x in range(matrix[0])] for x in range(matrix[1])]
-  mat[0] = [x for x in range(matrix[0])] 
-  for i in range(matrix[1]-1):
-   mat[i+1][0] = i+1
-  return mat
- def grade(self, a, b):
-  if a==b:
-   return copy
-  else:
-   return sub
- def score(self, a,b,i,j,s):
-  s1=s[i-1][j-1]+grade(a,b)
-  s2=s[i][j-1]+indel
-  s3=s[i-1][j]+indel
-  return min(s1,s2,s3)
- def calDistance(self):
-  self.__row = len(self.__seq1)
-  self.__col = len(self.__seq2)
-  self.mat = self.Matrix((self.__row+1,self.__col+1))
-  for i in range(self.__row):
-   for j in range(self.__col):
-    self.mat[i+1][j+1] = self.score(self.__seq1[i],self.__seq2[j],i+1,j+1,self.__mat)
-  return self.mat
-  
-
-wonji = EditDistance("GCTGA","GGGTGA")
-wonji.calDistance()
-
-
-
-class EditDistance(object):
- def __init__(self, seq1, seq2):
-  self.seq1=seq1
-  self.seq2=seq2
- def Matrix(self, matrix):
-  mat=[[0 for x in range(matrix[1])] for x in range(matrix[0])]
-  mat[0] = [x for x in range(matrix[1])] 
+  self.__mat=[[0 for x in range(matrix[1])] for x in range(matrix[0])]
+  self.__mat[0] = [x for x in range(matrix[1])] 
   for i in range(matrix[0]-1):
-   mat[i+1][0] = i+1
-  return mat
+   self.__mat[i+1][0] = i+1
+  return self.__mat
  def grade(self, a, b):
   if a==b:
    return 0 #copy
@@ -59,16 +27,22 @@ class EditDistance(object):
   s3=s[i-1][j]+1 #deletion
   return min(s1,s2,s3)
  def calDistance(self):
-  self.row = len(self.seq1)
-  self.col = len(self.seq2)
-  self.mat = self.Matrix((self.row+1,self.col+1))
-  for i in range(self.row):
-   for j in range(self.col):
-    self.mat[i+1][j+1] = self.score(self.seq1[i],self.seq2[j],i+1,j+1,self.mat)
-  return self.mat[self.row][self.col]
+  self.__row = len(self.__seq1)
+  self.__col = len(self.__seq2)
+  self.__mat = self.Matrix((self.__row+1,self.__col+1))
+  for i in range(self.__row):
+   for j in range(self.__col):
+    self.__mat[i+1][j+1] = self.score(self.__seq1[i],self.__seq2[j],i+1,j+1,self.__mat)
+  return self.__mat[self.__row][self.__col]
  def getMatrix(self):
-  self.mat
+  return self.__mat
+ def showResult(self):
+  self.result=['Edit Distance Matrix']
+  for line in self.__mat: self.result.append(str(line))
+  self.result.extend([' ','Minimum Distance :'+' '+str(self.calDistance())])
+  print '\n'.join(self.result)
 
 
-
-
+wonji = EditDistance("GCTGA","GGGTGA")
+wonji.calDistance()
+wonji.showResult()
